fcfad09d9d5dfcb442847920fc50579e846563aa
AndresGS <agayososegui@iessonferrer.net>
2021-10-06 20:07:45 +0200

    php

--- CristmasTree.php
+++ CristmasTree.php
@@ -10,10 +10,54 @@
     </label>
     <input type="submit"/>
 </form>
-<div>
+<div style="background-color: skyblue; width:min-content">
     <?php
     if (isset($_POST["num"])) {
         $num = intval($_POST["num"]);
+
+        $count = $num;
+        $espacio1 = true;
+        $espacio2 = true;
+
+        for ( $i=0; $i<$num; $i++ ) {
+
+            for ($j=0; $j<$i; $j++) {
+
+                if ($espacio1==true){
+                    for( $x=0;$x < $count ; $x++ ) {
+
+                        echo "z";
+
+                    }
+
+                    $espacio1 = false;
+                }
+
+                echo "*";
+
+            }
+            for ($z=0; $z<$i; $z++) {
+
+                echo "*";
+                if ($espacio2==true) {
+                    for ($y = 0; $y < $count; $y++) {
+
+                        echo "z";
+
+                    }
+
+                    $espacio2 = false;
+                }
+
+            }
+
+            echo "<br/>";
+            $count--;
+            $espacio1 = true;
+            $espacio2 = true;
+        }
+
+        //<span style='color:skyblue'>
 
     }
     ?>
