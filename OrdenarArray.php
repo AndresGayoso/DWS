0c266047f99d89cae85d5ac2f5a43a80c6c4d086
AndresGS <agayososegui@iessonferrer.net>
2021-10-13 19:35:59 +0200

    php

--- OrdenarArray.php
+++ OrdenarArray.php
@@ -8,11 +8,35 @@
 </form>
 <div>
     <?php
+
+    function OrdenarArray(){
+
+        $ordenar = array(1,5,8,0,3,2,4,7,9,6);
+        $cambiar = 0;
+        $z = 1;
+
+        for ($i = 0; $i < count($ordenar); $i++){
+            for ($x = 0; $z != 10; $x++) {
+                if ($ordenar[$x] > $ordenar[$z]) {
+                    $cambiar = $ordenar[$x];
+                    $ordenar[$x] = $ordenar[$z];
+                    $ordenar[$z] = $cambiar;
+                }
+                $z++;
+            }
+            $z = 1;
+        }
+
+        var_dump($ordenar);
+
+    }
+
+
+
     if (isset($_POST["btn"])) {
         $num = intval($_POST["btn"]);
 
-
-
+        OrdenarArray();
 
 
     }
