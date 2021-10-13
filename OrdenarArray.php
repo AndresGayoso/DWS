b0539779b4d90b134a0ce7601d475035ee833e0c
AndresGS <agayososegui@iessonferrer.net>
2021-10-13 20:03:04 +0200

    php

--- OrdenarArray.php
+++ OrdenarArray.php
@@ -14,22 +14,19 @@
         $ordenar = array(1,5,8,0,3,2,4,7,9,6);
         /*Variable que se utiliza de apoyo para cambiar un numero de posicion*/
         $cambiar = 0;
-        /*Variable que se utiliza para ver el numero siguiente al que estamos mostrando*/
-        $z = 1;
 
         /*Bucle principal para ordenadar el array comopleto*/
         for ($i = 0; $i < count($ordenar); $i++){
             /*Bucle que va ordenando los numero*/
-            for ($x = 0; $z != 10; $x++) {
+            for ($x = 0; $x < count($ordenar); $x++) {
                 /*Condicion que si el numero siguiente es menor lo cambie por el actual*/
-                if ($ordenar[$x] > $ordenar[$z]) {
-                    $cambiar = $ordenar[$x];
-                    $ordenar[$x] = $ordenar[$z];
-                    $ordenar[$z] = $cambiar;
+                if ($ordenar[$i] < $ordenar[$x]) {
+                    $cambiar = $ordenar[$i];
+                    $ordenar[$i] = $ordenar[$x];
+                    $ordenar[$x] = $cambiar;
                 }
-                $z++;
+
             }
-            $z = 1;
         }
         /*Mostrar array*/
         var_dump($ordenar);
