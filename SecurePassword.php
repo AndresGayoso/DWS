714e5ab7aaac2b10373c0b9259be6f605bc419c5
AndresGS <agayososegui@iessonferrer.net>
2021-10-13 20:30:38 +0200

    php

--- SecurePassword.php
+++ SecurePassword.php
@@ -13,16 +13,64 @@
 <div>
     <?php
 
-    function PasswordCheck($passw){
-        $letras = 0;
-        $datos = count_chars($passw, 1);
-        foreach ($datos as $key => $value)
+    /* Encontrar el numero de caracteres(no repetidos)
+
+     foreach ($datos as $key => $value)
         {
             $key = chr($key);
             $letras++;
+        }*/
+
+    function PasswordCheck($passw){
+
+        /*Varibles que cambian los segundos a dias,semanas,mese y años*/
+        $minutos = 0;
+        $hora = 0;
+        $dia = 0;
+        $dias = 0;
+        $semana = 0;
+        $mes = 0;
+        $año = 0;
+
+        /*Cuanto tiempo en segundo tardan en piratear la contraseña*/
+        $tiempo = round(pow(256,strlen($passw))/1000);
+
+        while($tiempo > 60){
+
+            if($tiempo > 60){
+                $minutos++;
+                $tiempo -= 60;
+            }
+
+            if($minutos == 60 ){
+                $minutos = 0;
+                $hora++;
+            }
+
+            if($hora == 24){
+              $hora = 0;
+              $dia++;
+              $dias++;
+            }
+
+            if($dia == 7){
+                $semana++;
+                $dia = 0;
+            }
+
+            if($dias == 31){
+                $mes++;
+                $dias = 0;
+            }
+
+            if($mes == 12){
+                $año++;
+                $mes = 0;
+            }
+
         }
 
-
+        echo $año," años ",$mes," meses ",$semana," semanas ",$dia," dias ",$hora," horas ",$minutos," minutos ",$tiempo," segundos";
 
     }
 
