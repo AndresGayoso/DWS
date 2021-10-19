8b1fe24edc5c25b8daf9acb44b4c112aee64e95e
AndresGS <agayososegui@iessonferrer.net>
2021-10-19 19:47:43 +0200

    php

--- world.php
+++ world.php
@@ -2,17 +2,38 @@
 $contents = file_get_contents("https://dawsonferrer.com/allabres/apis_solutions/world.php?data=world");
 $world = json_decode($contents, true);
 
-function getUnsortedCities($world){
+function getUnsortedCities($cities){
     //TODO: Return an array of cities without any kind of sort.
     //NOTES 1: You receive a world multidimensional array, you can view it's content with var_dump() function.
     //NOTES 2:You CAN'T use any sorting PHP built-in function.
-    return world;
+    for ($i = 0; $i < count($cities);$i++){
+        for ($x = 0; $x < count($cities[$i]['Cities']);$x++){
+            $ciudades[] = $cities[$i]['Cities'][$x];
+        }
+    }
+    return $ciudades;
+
 }
 
 function getSortedCitiesByPopulation($cities){
     //TODO: Return an array of cities sorted by it's population (ascending order).
     //NOTES 1: You receive a cities multidimensional array, you can view it's content with var_dump() function.
     //NOTES 2:You CAN'T use any sorting PHP built-in function.
+    $ciudades = getUnsortedCities($cities);
+    $ordenado = array();
+
+    for ($i = 0; $i < count($ciudades);$i++){
+        for ($x = 0; $x < count($ciudades);$x++){
+            if ($ciudades[$i]['Population'] < $ciudades[$x]['Population']){
+                $ordenado = $ciudades[$i];
+                $ciudades[$i] = $ciudades[$x];
+                $ciudades[$x] = $ordenado;
+            }
+        }
+    }
+
+    return $ciudades;
+
 }
 ?>
 <html lang="es">
@@ -57,9 +78,33 @@ function getSortedCitiesByPopulation($cities){
     <tbody>
     <?php
     //TODO: Logic to print the table body.
-    echo ('<pre>');
-    echo var_dump($world);
-    echo ('</pre>');
+    $ordenado = getSortedCitiesByPopulation($world);
+    $desordenado = getUnsortedCities($world);
+    $count = 0;
+    for ($i = 0; $i < count($ordenado);$i++){
+        echo '<tr>';
+        //desordenada
+        echo '<td>';
+        echo $desordenado[$i]['ID'];
+        echo '</td>';
+        echo '<td>';
+        echo $desordenado[$i]['Name'];
+        echo '</td>';
+        echo '<td>';
+        echo $desordenado[$i]['Population'];
+        echo '</td>';
+        //ordenada
+        echo '<td>';
+        echo $ordenado[$i]['ID'];
+        echo '</td>';
+        echo '<td>';
+        echo $ordenado[$i]['Name'];
+        echo '</td>';
+        echo '<td>';
+        echo $ordenado[$i]['Population'];
+        echo '</td>';
+        echo '</tr>';
+    }
     ?>
     </tbody>
 </table>
