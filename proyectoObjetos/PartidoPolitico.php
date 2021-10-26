8b52117666dfe04ebf9619dd1ee993f11f09d9b2
AndresGS <agayososegui@iessonferrer.net>
2021-10-26 20:54:29 +0200

    php

--- proyectoObjetos/PartidoPolitico.php
+++ proyectoObjetos/PartidoPolitico.php
@@ -5,13 +5,45 @@ class PartidoPolitico{
     private $nombre;
     private $provincia;
     private $votos;
+    private $escanos;
 
-    function __constructor($nombre,$provincia,$votos){
+    function __constructor($nombre,$provincia,$votos,$escanos){
 
         $this->nombre=$nombre;
         $this->provincia=$provincia;
         $this->votos=$votos;
+        $this->escanos=$escanos;
 
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getProvincia()
+    {
+        return $this->provincia;
+    }
+
+    public function getVotos()
+    {
+        return $this->votos;
+    }
+
+    public function getEscanos()
+    {
+        return $this->escanos;
+    }
+
+    public function setVotos($votos)
+    {
+        $this->votos = $votos;
+    }
+
+    public function setEscanos($escanos)
+    {
+        $this->escanos = $escanos;
     }
 
 
--- proyectoObjetos/main.php
+++ proyectoObjetos/main.php
@@ -1,1 +1,21 @@
 <?php
+include ("PartidoPolitico.php");
+
+$partidos = ["PP","PSOE","PACMA"];
+
+$provincias = ["Madrid","Barcelona","Valencia","Alicante","Sevilla","Málaga","Murcia","Cádiz","Baleares","La Coruña","Las Palmas","Vizcaya","Asturias","Granada","Pontevedra","Santa Cruz de  Tenerife","Zaragoza","Almería","Badajoz","Córdoba","Gerona","Guipúzcoa","Tarragona","Toledo","Cantabria","Castellón","Ciudad Real","Huelva","Jaén","Navarra","Valladolid","Álava","Albacete","Burgos","Cáceres","León","Lérida","Lugo","Orense","La Rioja","Salamanca","Ávila","Cuenca","Guadalajara","Huesca","Palencia","Segovia","Teruel","Zamora","Soria","Ceuta","Melilla"];
+
+function createPartidos(){
+    global $partidos;
+    global $provincias;
+
+    /*
+    for ($i = 0; $i < count($provincias);$i++){
+        for ($x = 0; $x < count($partidos)){
+
+        }
+    }
+    */
+
+
+}
