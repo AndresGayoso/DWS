a796781d19686b957d529392dcce385be6db714e
AndresGS <agayososegui@iessonferrer.net>
2022-01-14 19:00:59 +0100

    php

--- MVC Hoteles/DB/db.php
+++ MVC Hoteles/DB/db.php
@@ -14,12 +14,18 @@ class db extends mysqli{
      * @param $database
      */
 
-    public function local()
+    public function conexion(){
+        $this->conectar();
+    }
+
+    public function conectar()
     {
         parent::__construct($this->servername, $this->username, $this->password, $this->database);
 
         if (mysqli_connect_error()) {
             die("ERROR DATABASE: " . mysqli_connect_error());
+        }else{
+            echo "hola";
         }
     }
 
