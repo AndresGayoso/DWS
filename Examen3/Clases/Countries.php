750373c8fe227d7b571260531a747b49695dfa34
AndresGS <agayososegui@iessonferrer.net>
2022-02-01 17:09:56 +0100

    php

---
+++ Examen3/Clases/Countries.php
@@ -0,0 +1,99 @@
+<?php
+
+class Countries
+{
+
+    private string $code;
+    private string $name;
+    private int $population;
+    private int $gnp;
+    private int $numLanguages;
+    private int $numcities;
+    private string $owner;
+
+    /**
+     * @param string $code
+     * @param string $name
+     * @param int $population
+     * @param int $gnp
+     * @param int $numLanguages
+     * @param int $numcities
+     * @param string $owner;
+     */
+    public function __construct(string $code, string $name, int $population, int $gnp, int $numLanguages, int $numcities, string $owner)
+    {
+        $this->code = $code;
+        $this->name = $name;
+        $this->population = $population;
+        $this->gnp = $gnp;
+        $this->numLanguages = $numLanguages;
+        $this->numcities = $numcities;
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPopulation(): int
+    {
+        return $this->population;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGnp(): int
+    {
+        return $this->gnp;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumLanguages(): int
+    {
+        return $this->numLanguages;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumcities(): int
+    {
+        return $this->numcities;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOwner(): string
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param int $owner
+     */
+    public function setOwner(string $owner): void
+    {
+        $this->owner = $owner;
+    }
+
+
+}
