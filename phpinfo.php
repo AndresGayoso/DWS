9e7e4e812f7b33e44f76c565e1a2df283f165d33
Andres GS <andresitooctubre@gmail.com>
2022-05-17 18:13:29 +0200

    php

--- phpinfo.php
+++
@@ -1,2 +0,0 @@
-<?php
-    echo date( "Y-m-d",strtotime(date( "Y-m-d" )."+1 year"));
--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hola = password_hash("hola",PASSWORD_DEFAULT);
-
-if(password_verify("holia",$hola)){
-    echo "esta bn";
-}else{
-    echo "esta mal";
-}
